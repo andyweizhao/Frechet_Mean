834fd9b4935388a7946277fed9a26ea337ccfc2c
mathisbirken <94528634+mathisbirken@users.noreply.github.com>
2023-03-13 13:51:48 +0100

    Add files via upload

---
+++ spd_frechet_mean.py
@@ -0,0 +1,41 @@
+import numpy as np
+import scipy
+
+
+def frechet_mean(x):
+    mu = x[0]
+    for _ in range(10):
+        delta_mu = sum(riemannian_log(mu, xi) for xi in x) / len(x)
+        new_mu = riemannian_exp(mu, delta_mu)
+        print(((mu - new_mu) ** 2).sum(), sum(dist_squared(xi, mu) for xi in x))
+        mu = new_mu
+    return mu
+
+
+def riemannian_log(P, Q):
+    sqrt_P = scipy.linalg.sqrtm(P)
+    sqrt_P_inv = scipy.linalg.sqrtm(np.linalg.inv(P))
+    return sqrt_P @ scipy.linalg.logm(sqrt_P_inv @ Q @ sqrt_P_inv) @ sqrt_P
+
+
+def riemannian_exp(P, Q):
+    sqrt_P = scipy.linalg.sqrtm(P)
+    sqrt_P_inv = scipy.linalg.sqrtm(np.linalg.inv(P))
+    return sqrt_P @ scipy.linalg.expm(sqrt_P_inv @ Q @ sqrt_P_inv) @ sqrt_P
+
+
+def dist_squared(P, Q):
+    #print(np.linalg.eigh(np.linalg.inv(P) @ Q)[0])
+    return (np.log(np.linalg.eigh(np.linalg.inv(P) @ Q)[0]) ** 2).sum()
+
+
+def random_spd_matrix(n, max_eigenvalue=1):
+    D = np.diag(np.random.rand(n) * max_eigenvalue + 1e-5)
+    M = scipy.stats.ortho_group.rvs(dim=n)
+    return M @ D @ M.transpose()
+
+
+n = 20
+t = 8
+x = [random_spd_matrix(n) for _ in range(t)]
+frechet_mean(x)
